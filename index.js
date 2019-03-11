6ed3a79528b82d74ab41515c0c11010c918f5bc2
Tobias Lang <t.lang@i6m.de>
2019-03-11 11:51:40 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,15 @@
+class People {
+constructor(name) {
+      this.name = name;
+    }
+    get Name() {
+      return this.name;
+    }
+    set Name(name) {
+      this.name = name;
+    }
+}
+let person = new People("Jon Snow");
+console.log(person.Name);
+person.Name = "Dany";
+console.log(person.Name);
