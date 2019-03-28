d27e098ad804c0e9bd2dc6091ba09b06e9bdab3e
Tobias Lang <t.lang@i6m.de>
2019-03-28 15:56:23 +0100

    Update index.js

--- index.js
+++ index.js
@@ -13,3 +13,11 @@ let person = new People("Jon Snow");
 console.log(person.Name);
 person.Name = "Dany";
 console.log(person.Name);
+
+var months = ['March', 'Jan', 'Feb', 'Dec'];
+months.sort();
+console.log(months);
+// expected output: Array ["Dec", "Feb", "Jan", "March"]
+
+const wahrheit = true;
+const liege = false;
